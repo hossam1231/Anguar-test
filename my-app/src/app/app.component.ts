4d32189aea893879c30bf3b6d1bca52a69cf7e19
Hossam1231 <Hossamsulleman@gmail.com>
2022-07-29 14:36:35 +0100

    reverted to old version and fixed

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,25 +1,39 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, AfterViewInit, ViewChild,Output,EventEmitter } from '@angular/core';
+import { DogBreedDropdownComponent } from './components/dropdown/dog-breed-dropdown/dog-breed-dropdown.component';
 
 @Component({
   selector: 'app-root',
   // tag used to embed component
-  template:`
-
-
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
+  showDiv = {
+   dropdown: false,
+  }
+
+@ViewChild(DogBreedDropdownComponent) child : any  ;
+
+@Output() messageEvent1 = new EventEmitter<string>();
 
 
-
+sendMessage(message:any) {
+  console.log("message sent");
+  this.messageEvent1.emit(this.message);
+}
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+this.message = this.child.message;
   }
 
+  message : string = "default";
 
+  receiveMessage($event: any) {
+    this.message = $event;
+    this.sendMessage(this.message)
+  }
 
 }
